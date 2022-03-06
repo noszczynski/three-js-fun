9b420c3903915cf4974e91b810660a206836a651
adamnoszczynski <adam.noszczynski@gmail.com>
2022-03-06 18:18:55 +0100

    initial THREE.js config

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,15 @@
+import T from "./three.js";
+
 const handleLoad = () => {
-    const app = document.querySelector("#app");
-    app.innerHTML = "Hejka";
+    const scene = new T.Scene();
+    const camera = new T.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    console.log(scene);
+    console.log(camera);
+
+    const renderer = new T.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 };
 
 window.addEventListener("load", handleLoad);
